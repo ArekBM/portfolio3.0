6b2c1b7821505b872e7a4036419f64f0c7e44b9d
ArekBM <eric.bmiyake@gmail.com>
2023-12-09 01:20:31 -0800

    small text

--- app/(components)/Nav.tsx
+++ app/(components)/Nav.tsx
@@ -13,9 +13,11 @@ const Nav = ({isInSection} : any) => {
 
     const bgColor = isInSection ? 'bg-offblack' : 'bg-nav'
     const txtColor = isInSection ? 'text-offwhitetxt' : 'text-black'
+    const btnColor = isInSection ? 'bg-nav' : 'bg-contactbtn'
+    const btnTxt = isInSection  ? 'text-black' : 'text-offwhitetxt'
 
     return (
-        <header className={`fixed top-0 z-50 flex w-full -translate-y-1 items-center justify-between ${bgColor} px-5 py-3`}>
+        <header className={`fixed top-0 z-50 flex w-full -translate-y-1 items-center justify-between ${bgColor} px-5 py-3 transition-all duration-500 ease-in-out`}>
             <Link to='home' smooth>
                 <h1 className={generalSans.className + `relative z-10 group text-[17.6px] ${txtColor}`}>Arek</h1>
             </Link>
@@ -45,9 +47,9 @@ const Nav = ({isInSection} : any) => {
                             </span>                       
                         </span>
                     </Link>
-                    <div className='p-2.5 bg-contactbtn rounded-full'>
+                    <div className={`p-2.5 ${btnColor} rounded-full transition-all duration-500 ease-in-out`}>
                         <Link to='contact' smooth>
-                            <span className={Cabinet.className + ' text-offwhitetxt relative inline-block'}>
+                            <span className={Cabinet.className + ` ${btnTxt} relative inline-block transition-all duration-500 ease-in-out`}>
                                 <span className='relative z-10 group text-[17.6px]'>
                                     contact me
                                     <span className='absolute inset-x-0 top-1/2 h-1 bg-darkgrey w-full transform origin-left scale-x-0 transition-transform duration-300 ease-in-out rounded-full group-hover:scale-x-100'></span>
